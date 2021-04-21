328b8bc34be616401c2da2ffcc05de1756ef90a4
jgesc <37676259+jgesc@users.noreply.github.com>
2021-04-21 20:23:46 +0100

    RV32I-Single-Core: Program counter

---
+++ rv32i-single-cycle/src/pc.v
@@ -0,0 +1,29 @@
+module ProgramCounter
+#(
+  parameter integer WIDTH = 30
+)
+(
+  input   logic [WIDTH-1:0] jmp_addr,
+  input   logic             set,
+  input   logic             clk,
+  output  logic [WIDTH-1:0] out
+);
+
+  // PC Memory
+  logic [WIDTH-1:0] pc;
+
+  // Next instruction
+  logic [WIDTH-1:0] n_pc;
+
+  // Positive clock edge
+  always @ ( posedge clk ) begin
+    out <= pc;
+    n_pc <= pc + 1;
+  end
+
+  // Negative clock edge
+  always @ ( negedge clk ) begin
+    pc <= set ? jmp_addr : n_pc;
+  end
+
+endmodule
