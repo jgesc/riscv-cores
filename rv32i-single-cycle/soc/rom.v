1ee980f8ea8f447bf7a2d28283a7729556f334f0
jgesc <37676259+jgesc@users.noreply.github.com>
2021-05-02 13:24:30 +0100

    RV32I-Single-Core: ROM

---
+++ rv32i-single-cycle/soc/rom.v
@@ -0,0 +1,15 @@
+module ROM
+#(
+  parameter  SIZE  = 8192,
+  parameter  WIDTH = 32
+)
+(
+  input   [WIDTH-1:0] addr,
+  output  [WIDTH-1:0] out
+);
+
+logic [WIDTH-1:0] data [0:SIZE-1];
+
+assign out = data[addr];
+
+endmodule // ROM
